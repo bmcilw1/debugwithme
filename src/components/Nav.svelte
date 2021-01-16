fef00eefec72d5e60a6657870eff06a1ac8241d3
Brian McIlwain <technicallyemployed@gmail.com>
2021-01-16 14:54:27 -0600

    Capitalized links

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -19,12 +19,12 @@
 
 	  <div id="menu" class="navbar-menu { showDropdown ? 'is-active' : ''}">
 		<div class="navbar-start">
-			<a on:click="{() => showDropdown = !showDropdown}" class="navbar-item" aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a>
-			<a on:click="{() => showDropdown = !showDropdown}" class="navbar-item" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a>
+			<a on:click="{() => showDropdown = !showDropdown}" class="navbar-item" aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a>
+			<a on:click="{() => showDropdown = !showDropdown}" class="navbar-item" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">About</a>
 
 			<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 				the blog data when we hover over the link or tap it on a touchscreen -->
-			<!-- <a on:click="{() => showDropdown = !showDropdown}" class="navbar-item" rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a> -->
+			<!-- <a on:click="{() => showDropdown = !showDropdown}" class="navbar-item" rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">Blog</a> -->
 		</div>
 	  </div>
 	
