afdaff18e1424e4eab5d0fb4b5158b74f2ca4cf8
Brian McIlwain <technicallyemployed@gmail.com>
2021-01-16 15:14:24 -0600

    More messaging

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,3 +7,17 @@
     <h2 class="subtitle">Let's tackle this one together</h2>
   </div>
 </section>
+
+<!-- Problem -->
+<section id="problem" class="section hero is-medium">
+  <div class="hero-body container">
+    <h1 class="title">Debugging can be</h1>
+    <h2 class="subtitle">Time Consuming</h2>
+    <h2 class="subtitle">Stressful</h2>
+    <h2 class="subtitle">Unpredictable</h2>
+    <h2 class="subtitle">...Just plain hard</h2>
+  </div>
+  <div class="container">
+    <div class="is-size-3">Yet, we spend so much of our time doing it.</div>
+  </div>
+</section>
